8370776f6c783d2d6ff270eeaace9d3f30bb71bd
Dysterio <ddysterio@gmail.com>
2026-07-22 23:11:00 +1200

    Change operator and command argument colors for pwsh

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,1 +1,9 @@
 oh-my-posh init pwsh --config C:/Projects/dotfiles/ohmyposh/config.toml | Invoke-Expression
+
+$PSStyle.FileInfo.Directory = $PSStyle.Foreground.BrightBlue
+Set-PSReadLineOption -Colors @{
+    Parameter = "#fe8019"
+    Operator = "#fabd2f"
+}
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
